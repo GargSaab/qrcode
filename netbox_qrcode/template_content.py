8bc11ceedfeab5151d525d650dc8301c6a2839f5
GargSaab <gargk753@gmail.com>
2023-02-07 12:15:52 +0530

    NEwCommit1

--- netbox_qrcode/template_content.py
+++ netbox_qrcode/template_content.py
@@ -14,7 +14,6 @@ class QRCode(PluginTemplateExtension):
         config = self.context['config']
         obj = self.context['object']
         request = self.context['request']
-        print(obj)
 	url = obj
         # get object settings
         obj_cfg = config.get(self.model.replace('dcim.', ''))
